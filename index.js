143e6f6f7c7d9bd148445f3118589114e7dbc674
Jonathan Leder <jonathan.roettger@web.de>
2022-11-15 10:53:36 +0100

    create function createCharacter

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
